2b79139494f6a4af8fe761296cafe309a5ceea34
neurosenya <mrsena1206@gmail.com>
2020-11-05 17:10:54 +0200

    moved functions to separate file

---
+++ chapter1/spike_generator.py
@@ -0,0 +1,119 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+plt.style.use('seaborn-whitegrid')
+
+
+def HomogeneousPoisson(rate, duration, dt=1e-3):
+    '''
+    Homogeneous Poisson process spike generator,
+    implemented by dividing time into bins of and
+    calculating probability of spike generation at each bin
+
+    Return: array with spike times
+    '''
+    NoBins = int(duration / dt)
+    time = np.random.uniform(0, 1, NoBins)
+
+    # choose elements that have x_rand less than prob. of firing a spike
+    spikes_indices = np.nonzero(rate * dt > time)
+
+    # convert indices to time
+    spikes = (np.array(spikes_indices) / NoBins) * duration
+
+    return spikes.flatten()
+
+def HomogeneousPoissonEfficient(rate, duration):
+    '''
+    Hoomogeneous Poisson process spike generator,
+    implemented by estimating interspike intervals.
+
+    Return: array with spike times
+    '''
+    spikes = [0]
+
+    while spikes[-1] < duration:
+        spikes.append( spikes[-1] - np.log(np.random.rand()) / rate )
+
+    return np.array( spikes[1:-1] )
+
+def HomogeneousPoissonRefractory(rate, duration, tau):
+    '''
+    Homogeneous Poisson spike generator,
+    with dynamic refractoriness after the spike
+    '''
+    spikes = [0]
+    while spikes[-1] < duration:
+        spikes.append(spikes[-1] - np.log(np.random.rand()) / rate)
+    ToBeRemoved = []
+    for i in range(1, len(spikes)):
+        t = spikes[i]
+        t_prev = spikes[i-1]
+        # calculating how much rate has recovered after previous spike
+        new_rate = rate_recovery(t - t_prev, r0=rate, tau_ref=tau)
+        x_rand = random.random()
+        if new_rate / rate < x_rand:
+            ToBeRemoved.append(i)
+    spikes = np.delete( np.array(spikes), ToBeRemoved )
+    return spikes
+
+def rate_recovery(t, r0, tau_ref):
+    '''
+    Introduces exponential recovering of firing rate,
+    after firing a spike
+    Should be used with Inhomogeneous Poisson process
+    '''
+    tau_ref /= 1000 # converts to seconds
+    return r0 * (1 - np.exp(-t/tau_ref) )
+
+def distribution_spike_counts(spikes, step_interval=100, bindwidth=1, plot=False):
+    spikes_counts = spike_count(spikes, step_interval)
+    if plot:
+        plt.figure(dpi=100)
+        plt.hist(spikes_counts, bins='auto',
+                color='purple', ec='black')
+        plt.xlabel('no. of spikes in the interval')
+    else:
+        return spikes_counts
+
+def ISI_distribution(spikes):
+    plt.figure(dpi=100)
+    isi = np.diff(spikes) * 1000
+    plt.hist(isi, bins='auto', color='purple', ec='black')
+    plt.xlabel('ISI [ms]', fontsize=16)
+    plt.ylabel('no. of intervals', fontsize=16)
+
+def spike_count(spikes, step_interval=100):
+    '''
+    Calculates the number of spikes over duration of spikes
+    with a given step interval
+    '''
+    spikes_counts = []
+    step_interval /= 1000 # converts to seconds
+    start = 0
+    end = step_interval
+    while end <= np.max(spikes):
+        spikes_counts.append( len(spikes[ (spikes > start) & (spikes <= end) ] ) )
+        start += step_interval
+        end += step_interval
+    return np.array(spikes_counts)
+
+def fano(spikes):
+    '''
+    Computes Fanos factors for different intervals over
+    which spikes are counted.
+    duration/interval = no. of counts
+    F = Var(X) / mean(X)
+    '''
+    fanos = []
+    spikes = np.array(spikes)
+    for t in range(1, 100): # ms
+        spikes_counts = spike_count(spikes, step_interval=t)
+        fanos.append( np.var(spikes_counts) / np.mean(spikes_counts) )
+    return fanos
+
+def coefficient_variation(data):
+    '''
+    C_v = std(X) / mean(X)
+    '''
+    return np.std(data) / np.mean(data)
